7cb4e723cc4a13ba10834e680f39e0474cb76ab9
Ed <ea.litten@gmail.com>
2018-01-29 15:21:12 +0000

    Add tests

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,33 @@
+describe('Javabuzz', function() {
+
+  var javabuzz;
+
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
+  describe('knows when a number is',function() {
+    it('divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+    it('divisible by 3 and 5', function() {
+      expect(javabuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT',function() {
+    it('divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(5)).toBe(false);
+    });
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(3)).toBe(false);
+    });
+    it('divisible by 3 and 5', function() {
+      expect(javabuzz.isDivisibleByThreeAndFive(37)).toBe(false);
+    });
+  });
+
+});
