b448a818fa655ecea79819f23ce4ed420c565ae8
Ed <ea.litten@gmail.com>
2018-01-29 15:58:31 +0000

    Add fizzbuzz functionality

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -3,6 +3,7 @@ var Javabuzz = function() {};
 Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
   return (number % divisor === 0);
 };
+
 Javabuzz.prototype.isDivisibleByThree = function(number) {
   return this._isDivisibleBy(number, 3);
 };
@@ -12,8 +13,16 @@ Javabuzz.prototype.isDivisibleByFive = function(number) {
 Javabuzz.prototype.isDivisibleByFifteen = function(number) {
   return this._isDivisibleBy(number, 15);
 };
+
 Javabuzz.prototype.says = function(number) {
+  if (this.isDivisibleByFifteen(number)) {
+    return 'JavaBuzz';
+  }
   if (this.isDivisibleByThree(number)) {
     return 'Java';
   }
+  if (this.isDivisibleByFive(number)) {
+    return 'Buzz';
+  }
+  return number;
 };
